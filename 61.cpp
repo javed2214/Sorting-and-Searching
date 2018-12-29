682af3b3209e8ffa1a0669fd257338f5a771b3dd
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2018-12-29 23:22:11 +0530

    Add files via upload

---
+++ 61.cpp
@@ -0,0 +1,31 @@
+// Binary Search Algorithm
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int *a, int s, int e, int x){
+	
+	if(e>=s){
+		
+		int mid=(s+e)/2;
+		
+		if(a[mid]==x)
+			return mid;
+		if(x>a[mid])
+			return binarySearch(a,mid+1,e,x);
+		else
+			return binarySearch(a,s,mid-1,x);
+	}
+
+	return -1;
+}
+
+int main(){
+	int a[]={1,2,3,4,5,6,7,8,9,10};
+	int n=sizeof(a)/sizeof(int);
+	int x=1;
+	int p=binarySearch(a,0,n-1,x);
+	cout<<"Index: "<<p;
+
+	return 0;
+}
